5d71ad73ec71b9d311de3bf034a593d08726d43d
Elliot Gaulin <2039854@carrefour.cegepvicto.ca>
2023-04-20 10:06:42 -0400

    update du gitignore

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,6 @@ import { AppModule } from './app.module';
 import * as fs from 'fs';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule, {
-  //   httpsOptions: {
-  //     key: fs.readFileSync('./src/secrets/key.pem'),
-  //     cert: fs.readFileSync('./src/secrets/cert.pem'),
-  //   },
-
-  // });
-
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
